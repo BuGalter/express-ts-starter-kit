c90a3d3698afa9b9eb411558b8278d80b9b11639
BuGalter <wardoc78@gmail.com>
2024-03-01 16:35:58 +0300

    feat: Added to project helmet delete: Delete types/joi, depricated change: Change init app and start api

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,18 @@
 import express, { Application } from 'express';
+import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 import * as swaggerDocument from './swagger/swagger.json';
 import { apiConfig } from './config';
-import { logger } from './logger';
 import { MainRouter } from './routes';
 
 const SWAGGER_PREFIX = '/docs';
+const API_PREFIX = apiConfig.apiPrefix;
+const swaggerPrefix = `${API_PREFIX}${SWAGGER_PREFIX}`;
 
-const init = () => {
-  const PORT = apiConfig.port;
-  const API_PREFIX = apiConfig.apiPrefix;
-  const HOST = apiConfig.host;
-  const swaggerPrefix = `${API_PREFIX}${SWAGGER_PREFIX}`;
+const app: Application = express();
 
-  const app: Application = express();
+app.use(swaggerPrefix, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(API_PREFIX, MainRouter);
+app.use(helmet());
 
-  app.use(swaggerPrefix, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-  app.use(API_PREFIX, MainRouter);
-
-  app.listen(PORT, () =>
-    logger.debug(` REST API server ready at: ${HOST}:${PORT}${API_PREFIX}`)
-  );
-};
-
-export { init };
+export { app };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
-import * as main from './api';
+import { app } from './api';
 import { logger } from './api/logger';
+import { apiConfig } from './api/config';
 
-try {
-  //throw new Error('test error');
-  main.init();
-} catch (error) {
-  logger.error(`REST API crash: ${error}`);
-}
+const PORT = apiConfig.port;
+const HOST = apiConfig.host;
+const API_PREFIX = apiConfig.apiPrefix;
+
+app
+  .listen(PORT, () => {
+    logger.debug(`REST API server ready at: ${HOST}:${PORT}${API_PREFIX}`);
+  })
+  .on('error', (error) => {
+    logger.error(`API crash: ${error}`);
+  });
